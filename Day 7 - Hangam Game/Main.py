f869af85e583792dd72f0c4eac78dce21b0a568a
guntasrikanth <guntasrikanth@gmail.com>
2023-09-06 18:01:23 +0530

    Update Main.py

--- Day 7 - Hangam Game/Main.py
+++ Day 7 - Hangam Game/Main.py
@@ -2,11 +2,13 @@ import random
 import words, hangman
 lives = 6
 end =True
+#word_list = ["aardvark", "baboon", "camel"]
 word_list = words.word_list
 choose_word = random.choice(word_list)
 
 length = len(choose_word)
 word = []
+guessed_word =[]
 for _ in range(length):
   word.append('_')
 
@@ -14,26 +16,30 @@ print(f"Welcome to {hangman.logo}\n\nPredict the word by guessing all the correc
 print(f"{' '.join(word)}\n")
 while end:
     letter = input("Guess a letter: ").lower()
-    for i in range(length):
-      if letter == choose_word[i]:
-        word[i] = letter
-    if '_' not in word:
-      end = False
-      print('you won')
-    if letter not in choose_word:
-      lives-=1
-      print(f"\nOpps!!!, You guessed the wrong letter. You have {lives} lives remaining.")
-      
-      if lives == 0:
-        end = False
-        print("You lose")
-        print("The word is: ",choose_word)
+    if letter in guessed_word:
+      print('You already guessed this letter, try another one.')
     else:
-      print("\nGreat, You guessed the correct letter. Keep going.")
-    print(f"{' '.join(word)}\n")
-    
-    print(hangman.stages[lives])
+      guessed_word.append(letter)
+      for i in range(length):
+        if letter == choose_word[i]:
+          word[i] = letter
+      if '_' not in word:
+        end = False
+        print('you won')
+      if letter not in choose_word:
+        lives-=1
+        print(f"\nOpps!!!, You guessed the wrong letter. You have {lives} lives remaining.")
+        if lives == 0:
+          end = False
+          print("You lose")
+          print("The word is: ",choose_word)
+      else:
+        print("\nGreat, You guessed the correct letter. Keep going.")
+      print(f"{' '.join(word)}\n")
+      
+      print(hangman.stages[lives])
 
+      
 """
 Connect with me on 
 [LinkedIn](www.linkedin.com/in/gunta-srikanth) 
