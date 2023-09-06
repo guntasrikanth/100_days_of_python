5091306b1fb988eb8cfd1eef971d8cc993c06179
guntasrikanth <guntasrikanth@gmail.com>
2023-09-06 17:41:25 +0530

    Added day7 project

---
+++ Day 7 - Hangam Game/Main.py
@@ -0,0 +1,35 @@
+import random 
+import words, hangman
+lives = 6
+end =True
+word_list = words.word_list
+choose_word = random.choice(word_list)
+
+length = len(choose_word)
+word = []
+for _ in range(length):
+  word.append('_')
+
+print(f"Welcome to {hangman.logo}\n\nPredict the word by guessing all the correct letters\n")
+print(f"{' '.join(word)}\n")
+while end:
+    letter = input("Guess a letter: ").lower()
+    for i in range(length):
+      if letter == choose_word[i]:
+        word[i] = letter
+    if '_' not in word:
+      end = False
+      print('you won')
+    if letter not in choose_word:
+      lives-=1
+      print(f"\nOpps!!!, You guessed the wrong letter. You have {lives} lives remaining.")
+      
+      if lives == 0:
+        end = False
+        print("You lose")
+        print("The word is: ",choose_word)
+    else:
+      print("\nGreat, You guessed the correct letter. Keep going.")
+    print(f"{' '.join(word)}\n")
+    
+    print(hangman.stages[lives])
