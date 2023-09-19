536ad67951f4ecd8117ac0db5838ac1861e37e1a
guntasrikanth <guntasrikanth@gmail.com>
2023-09-19 20:31:37 +0530

    Day 16 & 17 added

--- Day 17 - Quiz/data.py
+++ Day 17 - Quiz/data.py
@@ -1,14 +1,102 @@
 question_data = [
-{"text": "A slug's blood is green.", "answer": "True"},
-{"text": "The loudest animal is the African Elephant.", "answer": "False"},
-{"text": "Approximately one quarter of human bones are in the feet.", "answer": "True"},
-{"text": "The total surface area of a human lungs is the size of a football pitch.", "answer": "True"},
-{"text": "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", "answer": "True"},
-{"text": "In London, UK, if you happen to die in the House of Parliament, you are entitled to a state funeral.", "answer": "False"},
-{"text": "It is illegal to pee in the Ocean in Portugal.", "answer": "True"},
-{"text": "You can lead a cow down stairs but not up stairs.", "answer": "False"},
-{"text": "Google was originally called 'Backrub'.", "answer": "True"},
-{"text": "Buzz Aldrin's mother's maiden name was 'Moon'.", "answer": "True"},
-{"text": "No piece of square dry paper can be folded in half more than 7 times.", "answer": "False"},
-{"text": "A few ounces of chocolate can to kill a small dog.", "answer": "True"}
+    {
+        "category": "History",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "The Cold War ended with Joseph Stalin&#039;s death.",
+        "correct_answer": "False",
+        "incorrect_answers": [
+            "True"
+        ]
+    },
+    {
+        "category": "History",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "The Tiananmen Square protests of 1989 were held in Hong Kong.",
+        "correct_answer": "False",
+        "incorrect_answers": [
+            "True"
+        ]
+    },
+    {
+        "category": "History",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "The United States was a member of the League of Nations.",
+        "correct_answer": "False",
+        "incorrect_answers": [
+            "True"
+        ]
+    },
+    {
+        "category": "History",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "United States President John F. Kennedy was assassinated during his presidential motorcade in Atlanta, Georgia on November 22nd, 1963.",
+        "correct_answer": "False",
+        "incorrect_answers": [
+            "True"
+        ]
+    },
+    {
+        "category": "History",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "Former United States Presidents John Adams and Thomas Jefferson died within hours of each other.",
+        "correct_answer": "True",
+        "incorrect_answers": [
+            "False"
+        ]
+    },
+    {
+        "category": "History",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "Adolf Hitler was a german soldier in World War I.",
+        "correct_answer": "True",
+        "incorrect_answers": [
+            "False"
+        ]
+    },
+    {
+        "category": "History",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "In World War ll, Great Britian used inflatable tanks on the ports of Great Britain to divert Hitler away from Normandy/D-day landing.",
+        "correct_answer": "True",
+        "incorrect_answers": [
+            "False"
+        ]
+    },
+    {
+        "category": "History",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "Kublai Khan is the grandchild of Genghis Khan?",
+        "correct_answer": "True",
+        "incorrect_answers": [
+            "False"
+        ]
+    },
+    {
+        "category": "History",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "The United States of America was the first country to launch a man into space.",
+        "correct_answer": "False",
+        "incorrect_answers": [
+            "True"
+        ]
+    },
+    {
+        "category": "History",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "Thomas Crapper was a plumber who invented the flushing toilet.",
+        "correct_answer": "False",
+        "incorrect_answers": [
+            "True"
+        ]
+    }
 ]
---
+++ Day 17 - Quiz/main.py
@@ -0,0 +1,17 @@
+from data import question_data
+from question_model import Question
+from quiz_brain import QuizBrain
+
+question_bank = []
+for i in question_data:
+    question_text = i["question"]
+    answer = i["correct_answer"]
+    question_object = Question(question_text, answer)
+    question_bank.append(question_object)
+
+quiz = QuizBrain(question_bank)
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print("You have completed the quiz.")
+print(f"You final score is: {quiz.score}/{quiz.question_number}.")
